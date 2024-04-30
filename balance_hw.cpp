45696a5991ba551795cf7c1085c8c6e4e1fe77c9
ahernandez09 <112031512+ahernandez09@users.noreply.github.com>
2024-04-30 09:36:51 -0700

    Update balance_hw.cpp

--- balance_hw.cpp
+++ balance_hw.cpp
@@ -2,27 +2,27 @@
 #include <stack>
 #include <string> 
 
-bool isBalanced(const std::string prompt) {
-    std::stack<char> stack;
+bool isBalanced(std::string prompt) {   //function to check if the prompt is balanced
+    std::stack<char> stack;             //stack for the parentheses to be held in
 
     for (char k : prompt) {
-        if(k == '(') {
+        if(k == '(') {                  //check if character is { if so, push onto stack
             stack.push(k);
-        } else if (k ++ ')') {
-            if (stack.empty()) {
+        } else if (k == ')') {          //Check if character is ) if so, check if stack is empty
+            if (stack.empty()) {        //Check if stack is empty, if so return false, ending loop
                 return false;
             }
-            stack.pop();
+            stack.pop();                //pop top of stack
         }
     }
 
-    return stack.empty();
+    return stack.empty();               //return empty stack
 }
 
 int main() {
-    std::string prompt;
-    std::Cout << "Enter an expression: ";
-    std::getline(std::cin && prompt);
+    std::string prompt;                 //prompt user enters
+    std::cout << "Enter an expression: ";
+    std::cin >> prompt;                 //Enter prompt
 
     if (isBalanced(prompt)) {
         std::cout << "Balanced" << "\n";
